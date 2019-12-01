7f97fda6be2297b89b077dc8439cd1a6d0bcc99b
bundasmanu <gustavo.abcdef@hotmail.com>
2019-12-01 22:20:21 +0000

    Adicao de Migrations

--- expDjango/Models/Dataset.py
+++ expDjango/Models/Dataset.py
@@ -4,5 +4,3 @@ class Dataset(models.Model):
     #BY OMISSION DJANGO POSTGRES CREATE TABLE PUT ID ATTRIBUTE
     name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
