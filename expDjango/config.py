7af0e43700f111d6e0d910f67344cfedbaca4840
bundasmanu <gustavo.abcdef@hotmail.com>
2020-02-07 12:36:29 +0000

    Addition of configurations file. Created admin, and i create to groups (data_scientist and health_care) and set permissions for both.

---
+++ expDjango/config.py
@@ -0,0 +1,15 @@
+
+#THIS CLASS HAVE CONSTANT STRINGS THAT I USE ON PROJECT --> LIKE NAME OF GROUPS, PERMISSIONS, AND SO ON
+
+#GROUPS
+DATASCIENTIST_GROUP = "Data_Scientist"
+HEALTH_CARE_GROUP = "Health_Care_Professional"
+
+#APPLICATIONS
+DATASET = "Dataset"
+
+#PERMISSIONS
+ADD_DATASET = "Can add dataset"
+CHANGE_DATASET = "Can change dataset"
+DELETE_DATASET = "Can delete dataset"
+VIEW_DATASET = "Can view dataset"
