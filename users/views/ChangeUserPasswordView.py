c5b3dab3c3c3e9f24ef4942746d8b853e848ef9f
bundasmanu <gustavo.abcdef@hotmail.com>
2020-02-19 23:56:56 +0000

    Change password of an user, logic and template are concluded. I needed to do some additional research, in order to override custom PasswordChangeForm. All ref's and some things are described.

--- users/views/ChangeUserPasswordView.py
+++ users/views/ChangeUserPasswordView.py
@@ -7,31 +7,48 @@ from expDjango import config as config
 from django.urls import reverse
 from users.models import CustomUser
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
+from django.contrib.auth import update_session_auth_hash
+from django.core.exceptions import ObjectDoesNotExist
+
+#REFS:
+#https://gist.github.com/bmispelon/1851113
+#https://github.com/django/django/blob/2.0/django/contrib/auth/views.py#L566
+#https://docs.djangoproject.com/en/3.0/topics/auth/default/
+#https://stackoverflow.com/questions/36350317/django-authentication-issue-after-reseting-password
 
 class ChangeUserPasswordView(LoginRequiredMixin, UpdateView):
     form_class = PasswordChangeForm
     login_url = settings.LOGOUT_REDIRECT_URL
     template_name = 'users/ChangePassword.html'
     model = CustomUser.CustomUser
-    #pk_url_kwarg = 'pk'
 
     def get_object(self, queryset=None):
-        return self.model.objects.get(pk=self.request.user.id)
+        return self.request.user
 
     def get_context_data(self, *args ,**kwargs):
-        form =self.form_class(user=self.request.user)
-        kwargs = {'form' : form}
+        self.form_class = self.form_class(user=self.request.user)#INITIALIZATION OF PasswordChangeForm, NEEDS user instance
+        kwargs = {'form' : self.form_class, #PASS FORM TO TEMPLATE
+                  'user' : self.request.user}
         context = super().get_context_data(**kwargs)
         return context
+
+    def get_form_kwargs(self): #METHOD USED TO DEFINE ARGUMENTS TO INITIALIZE FORM
+        kwargs = super(ChangeUserPasswordView, self).get_form_kwargs()
+        kwargs['user'] = kwargs.pop('instance') #NEED TO PUT OBJECT INSTANCE ON KWARG USER, AND POP FUNCTION, DELETES INSTANCE KWARG --> https://stackoverflow.com/questions/49218302/python-difference-between-kwargs-pop-and-kwargs-get
+        return kwargs #KWARGS['USER'] IS NEEDED, TO FORM KNOW WHAT TYPE OF MODEL IS USED ON FORM, IN OTHER FORMS IN META, THE MODELS ARE DEFINED, IN THIS CASE SUCH AS A GENERIC FORM, I NEED TO DEFINE THIS
 
     def form_valid(self, form):
         try:
 
             #I DON'T NEED TO MAKE VALIDATION BECAUSE THIS VALIDATIONS ARE USED IN FORM: AdminPasswordChangeForm
             #I ONLY NEED TO CALL SAVE METHOD ON PasswordChangeForm FORM
-            self.form_class.save()
+            user = form.save() #RETURNS THE USER IF NEW PASS
 
-            #self.model.objects.filter(pk=user.id).update(password=user.password)
+            #CASE OBJECT IS NULL --> PROBLEMS ON CLEANED_DATA
+            if user == None:
+                raise ObjectDoesNotExist
+
+            update_session_auth_hash(self.request, user)#CHANGES AUTOMATICALLY THE PASSSWORD OF USER AND HIS SESSION, AND USER IS NOT LOGGED OUT
 
             return HttpResponseRedirect(self.get_success_url())
         except:
