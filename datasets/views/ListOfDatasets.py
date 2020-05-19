e78aa137d17b05358ca1779669e8375630c39540
bundasmanu <gustavo.abcdef@hotmail.com>
2020-05-19 17:22:31 +0100

    List of all datasets already concluded (scripts and styles), also links for each individual dataset.

--- datasets/views/ListOfDatasets.py
+++ datasets/views/ListOfDatasets.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
-from ..models import Dataset
-from django.db.models import Count
+from ..models import Dataset, CNNModel
+from django.db.models import Count, Subquery
+from django.db.models import F
 
 class ListOfDatasets(ListView):
 
@@ -10,5 +11,17 @@ class ListOfDatasets(ListView):
 
     # concatenate queryset's link: https://stackoverflow.com/questions/48872380/display-multiple-queryset-in-list-view
     def get_queryset(self):
-        queryset = Dataset.Dataset.objects.annotate(numbermodels=Count('cnnmodel'))
-        return queryset
+        datasets = Dataset.Dataset.objects.all().values('name',
+                                                                'normalize_std', 'normalize_mean', 'id')\
+                                                                .order_by('id').distinct('id')
+
+        return datasets
+
+    def get_context_data(self, **kwargs):
+        context = super(ListOfDatasets, self).get_context_data(**kwargs)
+        models_per_dataset = Dataset.Dataset.objects.values('id', 'cnnmodel__name').order_by('id')
+        context['models'] = models_per_dataset
+        number_models = Dataset.Dataset.objects.annotate(numbermodels=Count('cnnmodel'))
+        context['number_models'] = number_models
+        return context
+    #https://stackoverflow.com/questions/45242717/how-to-select-data-from-two-tables-in-django
