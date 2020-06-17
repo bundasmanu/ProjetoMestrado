f47c37bb9ae15d84e90030ac635ce010a900cc41
bundasmanu <gustavo.abcdef@hotmail.com>
2020-06-17 22:23:14 +0100

    Improved Predict Form, in order to avoid problem on migrations, for new environments.

--- datasets/forms/PredictForm.py
+++ datasets/forms/PredictForm.py
@@ -6,20 +6,26 @@ from .. import validators
 
 class PredictForm(forms.Form):
 
-    # get only dataset's with models associated
-    datasets_with_models = Dataset.Dataset.objects.all().annotate(numbermodels=Count('cnnmodel'))
-    datasets_with_models = datasets_with_models.exclude(numbermodels=0)
-
-    # choices of dropdown's
-    dataset_choices = [(x.id, x.name) for x in datasets_with_models]
-    dataset_choices.insert(0, ('', 'Selecciona um dataset'))
-    models_choices = []
-    models_choices.insert(0, ('', 'Selecciona um modelo'))
-
     # fields of form
     dataset_dropdown = forms.CharField(label="Dataset", empty_value='Selecciona um dataset',
-                                        widget=forms.Select(choices=dataset_choices), required=True)
+                                        widget=forms.Select(choices=[('', '')]), required=True)
     models_dropdown = forms.CharField(label="Model", empty_value='Selecciona um modelo',
-                                      widget=forms.Select(choices=models_choices), required=True)
+                                      widget=forms.Select(choices=[('', '')]), required=True)
     image_upload = forms.ImageField(max_length=config.MAX_IMAGE_SIZE, required=True, label='Seleccione uma imagem',
                                     help_text='Tamanho máximo 20MB', allow_empty_file=False, validators=[validators.validate_image_file_extension])
+
+    def __init__(self, *args, **kwargs): # need to populate dropdown's in init because this form applies queryset's and db logic, and interferes in migrations, for new environment, in this way, i don't have problems
+        super(PredictForm, self).__init__(*args, **kwargs)
+
+        # get only dataset's with models associated
+        datasets_with_models = Dataset.Dataset.objects.all().annotate(numbermodels=Count('cnnmodel'))
+        datasets_with_models = datasets_with_models.exclude(numbermodels=0)
+
+        # choices of dropdown's
+        dataset_choices = [(x.id, x.name) for x in datasets_with_models]
+        dataset_choices.insert(0, ('', 'Selecciona um dataset'))
+        models_choices = []
+        models_choices.insert(0, ('', 'Selecciona um modelo'))
+
+        self.fields["dataset_dropdown"].widget.choices = dataset_choices
+        self.fields["models_dropdown"].widget.choices = models_choices
